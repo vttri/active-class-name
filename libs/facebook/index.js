1c2c098e0939bced1d1d23f6890ab8679bd9cb7a
Tri van <tri.van@zodinet.com>
2022-07-29 15:36:29 +0700

    init code

--- libs/facebook/index.js
+++ libs/facebook/index.js
@@ -1,4 +1,11 @@
 // log specific events happening.
 export const event = ({ action, params = {} }) => {
-  window.fbq('track', "SubmitApplication");
+  fbq("track", "AddToCart", {
+    content_name: "Really Fast Running Shoes",
+    content_category: "Apparel & Accessories > Shoes",
+    content_ids: ["1234"],
+    content_type: "product",
+    value: 4.99,
+    currency: "USD",
+  });
 };
