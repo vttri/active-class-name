dd6035da6d074e33b934a31f1d1983ad2329bc80
Tri van <tri.van@zodinet.com>
2022-07-29 11:27:11 +0700

    update tiktok

--- pages/_document.js
+++ pages/_document.js
@@ -48,6 +48,23 @@ export default class MyDocument extends Document {
       src="https://www.facebook.com/tr?id=389979976455254&ev=PageView&noscript=1" />`,
             }}
           />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `!function (w, d, t) {
+                w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
+              
+                ttq.load('CBHM1CBC77U0O6L265R0');
+                ttq.page();
+                ttq.track('AddToCart',{  
+                    content_id: '301',
+                    quantity: 1,
+                    price: 8,
+                    value: 9.2,
+                    currency: 'USD',});
+              }(window, document, 'ttq');`,
+            }}
+          />
         </Head>
         <body>
           <Main />
