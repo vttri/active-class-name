21f6ac4cc102cd4adf8cd84c538abf07f3ecf68a
Tri van <tri.van@zodinet.com>
2022-07-29 15:03:37 +0700

    update fbker

--- libs/facebook/index.js
+++ libs/facebook/index.js
@@ -1,4 +1,5 @@
 // log specific events happening.
 export const event = ({ action, params = {} }) => {
-  window.fbq("track", "SubmitApplication");
+    window.fbq('init','389979976455254')
+  window.fbq('track', "SubmitApplication");
 };
