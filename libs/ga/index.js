a964825371dbc42b92441b37a9b9341abe030132
Tri van <tri.van@zodinet.com>
2022-08-01 10:33:10 +0700

    update gg

--- libs/ga/index.js
+++ libs/ga/index.js
@@ -1,5 +1,4 @@
-
 // log specific events happening.
 export const event = ({ action, params }) => {
-    window.dataLayer.push({'event': 'new_subscriber'});
-}
+  window.dataLayer.push({ event: "new_subscriber", params: params });
+};
