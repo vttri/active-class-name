512abe7c8c57f0a4f6228ac452430976f404480e
Tri van <tri.van@zodinet.com>
2022-08-01 10:11:17 +0700

    update gg

--- libs/ga/index.js
+++ libs/ga/index.js
@@ -1,11 +1,5 @@
-// log the pageview with their URL
-export const pageview = (url) => {
-    window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-        page_path: url,
-    })
-}
 
 // log specific events happening.
 export const event = ({ action, params }) => {
-    window.gtag('event', action, params)
+    window.dataLayer.push({'event': 'new_subscriber'});
 }
